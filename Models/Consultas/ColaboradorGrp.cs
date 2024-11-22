dc37b6a87f4cebd2aebf7d90af9f45e39c5af412
Mirelle Eduarda R. Oliveira <93480351+mirelleduarda@users.noreply.github.com>
2024-11-22 14:02:58 -0300

    Filtro de Colaboradores

---
+++ Models/Consultas/ColaboradorGrp.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_Casa_Criancas.Models.Consultas
+{
+    public class ColaboradorGrp
+    {
+        [Display(Name = "ID Colaborador: ")]
+        public int id { get; set; }
+
+        [Display(Name = "Nome do Colaborador: ")]
+        public string colaboradorNome { get; set; }
+
+        [Display(Name = "Nome do Curso: ")]
+        public string cursoNome { get; set; }
+
+        [Display(Name = "Descrição do Curso: ")]
+        public string cursoDescricao { get; set; }
+    }
+}
